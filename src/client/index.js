84e4f9acfb19f4b267a43da0e2fe4b4f0cf228ef
1885189 <1885189@sms.ed.ac.uk>
2020-04-19 12:28:44 +0200

    loaders and dist output

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,5 @@
+import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker'
+// add babel, translate one version of javascript to another
+console.log(checkForName);
 alert("I am alive!");
--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,12 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        // rules array contains all loader, each loader specifies what types of file  he runs
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    }
 };
